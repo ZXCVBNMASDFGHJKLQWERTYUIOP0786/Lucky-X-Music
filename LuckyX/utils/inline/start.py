610a8f11538112b60172fed424f5a75a3de98898
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-15 21:17:15 +0530

    Update start.py

--- LuckyX/utils/inline/start.py
+++ LuckyX/utils/inline/start.py
@@ -2,35 +2,47 @@ from pyrogram.types import InlineKeyboardButton
 
 import config
 from LuckyX import app
+from config import BOT_USERNAME , OWNER_ID
 
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_11"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
+            InlineKeyboardButton(
+                text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
-
 
 
 def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="𝐎ᴡɴᴇʀ", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="𝐃ᴇᴠʟᴏᴘᴇʀ", user_id=5493923823),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper")
+        ],
+        [
+            InlineKeyboardButton(text="𝐆𝚁𝙾𝚄𝙿✨", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="𝐌ᴏʀᴇ🥀", url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
