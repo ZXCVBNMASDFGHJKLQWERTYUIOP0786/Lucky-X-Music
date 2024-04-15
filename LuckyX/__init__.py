b99b0e9fbd8c86786b2250ad83b71d07ca253590
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-15 10:30:24 +0530

    Add files via upload

--- LuckyX/__init__.py
+++ LuckyX/__init__.py
@@ -1,1 +1,28 @@
+from LuckyX.core.bot import Raja
+from LuckyX.core.dir import dirr
+from LuckyX.core.git import git
+from LuckyX.core.userbot import Userbot
+from LuckyX.misc import dbb, heroku
 
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Raja()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
