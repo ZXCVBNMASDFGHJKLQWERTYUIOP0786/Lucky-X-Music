4c7b1e68c05e4510420fa058298b788483418947
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-16 14:54:32 +0530

    Update play.py

--- LuckyX/utils/inline/play.py
+++ LuckyX/utils/inline/play.py
@@ -108,22 +108,21 @@ def telegram_markup_timer(_, chat_id, played, dur):
     buttons = [
         
         [
-            InlineKeyboardButton(text=f"{played} {bar} {dur}", callback_data="GetTimer",)
-        ],
-          [
-            InlineKeyboardButton(text="▷ ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II ᴘᴀᴜsᴇ", callback_data=f"ADMIN Pause|{chat_id}",),
-            InlineKeyboardButton(text="sᴋɪᴘ ‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
         ],
         [
-            InlineKeyboardButton(text="▢ sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ ↺", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="❣️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="❤️", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="💞", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="💔", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="❤️‍🩹", callback_data=f"ADMIN Stop|{chat_id}")
+        
         ],
-        [
-            InlineKeyboardButton(text="๏ ᴍᴏʀᴇ ๏", callback_data=f"PanelMarkup None|{chat_id}",),
-        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
-
     return buttons
 
 def stream_markup(_, chat_id):
