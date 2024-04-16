a67b18af1045bc90e536c7a3e11c07ff6202ef65
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-16 21:09:49 +0530

    Update thumbnails.py

--- LuckyX/utils/thumbnails.py
+++ LuckyX/utils/thumbnails.py
@@ -1,11 +1,14 @@
+import asyncio
 import os
+import random
 import re
+import textwrap
 import aiofiles
 import aiohttp
-from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
-from unidecode import unidecode
+from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
+                 ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
-from LuckyX import app
+import numpy as np
 from config import YOUTUBE_IMG_URL
 
 def changeImageSize(maxWidth, maxHeight, image):
